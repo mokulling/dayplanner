621401c7a0f56844d740a7e2b7295c40c1d08292
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-09 16:57:33 -0600

    updated formatting

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var hour = moment().format('h')
 var description = $('.description')
 var hourRow = $('.hour')
 var saveBtn = $('.saveBtn')
-var input = $('<input>')
+var input = $('.col-10')
 var boxes = [$('.row-1'), $('.row-2'),$('.row-3'),$('.row-4'),$('row-5'),$('#row-6') ,$('.row-7'),$('.row-8'),$('.row-9'), ]
 var spans = $('<span>')
 var hours = [$('#9'), $('#10'), $('#11'), $('#12'), $('#13'),$('#14'),$('#15'),$('#16'),$('#17')]
+var times = $('.times')
 
 
 //for loop to change class
@@ -18,14 +19,17 @@ for (let i = 0; i < hours.length; i++) {
     var currentWork = hours[i];
     var workValue = currentWork[0].textContent
     console.log(workValue)
-    
     if (workValue == hour) {
         console.log('equal')
+        input.addClass('present')
+      
     } else if (workValue > hour) {
         console.log('greater')
+        input.addClass('past')
         
     } else {
         console.log('less than ')
+        input.addClass('future')
         
     }
     
