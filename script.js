e7229b53f94e0b3d9afe1ba885b12d79ba5d6bb7
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-09 13:40:10 -0600

    Create skeleton of calendar

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var currentDay = $('#currentDay')
+
+
+//if statement on past, present future,
+//if past change class to past
+//if present change class to present
+//if pfuture change class to future 
+//click listener to save to local storage
