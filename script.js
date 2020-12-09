e94f5eb56798b5cc59a0f8ad3dcfd8bbc570b4f2
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-09 16:24:33 -0600

    extracted time values

--- script.js
+++ script.js
@@ -1,8 +1,75 @@
 var currentDay = $('#currentDay')
+var month = moment().month()+1 ;
+var day = moment().date()
+var hour = moment().format('h')
+var description = $('.description')
+var hourRow = $('.hour')
+var saveBtn = $('.saveBtn')
+var input = $('<input>')
+var boxes = [$('.row-1'), $('.row-2'),$('.row-3'),$('.row-4'),$('row-5'),$('#row-6') ,$('.row-7'),$('.row-8'),$('.row-9'), ]
+var spans = $('<span>')
+var hours = [$('#9'), $('#10'), $('#11'), $('#12'), $('#13'),$('#14'),$('#15'),$('#16'),$('#17')]
+
+
+//for loop to change class
+
+console.log(hour)
+for (let i = 0; i < hours.length; i++) {
+    var currentWork = hours[i];
+    var workValue = currentWork[0].textContent
+    console.log(workValue)
+    
+    if (workValue == hour) {
+        console.log('equal')
+    } else if (workValue > hour) {
+        console.log('greater')
+        
+    } else {
+        console.log('less than ')
+        
+    }
+    
+}
 
 
 //if statement on past, present future,
 //if past change class to past
 //if present change class to present
 //if pfuture change class to future 
+
+//click listener to add in event
+
+description.on('click', function (event){
+    var thisSection = event.target
+    console.log(thisSection)
+    //thisSection.append(input.text)
+    
+})
 //click listener to save to local storage
+saveBtn.on('click', function(event) {
+    
+    console.log(event.target)
+    
+})
+
+
+//Insert current day into p id tag
+
+ 
+
+//console.log(month)
+//console.log(day)
+//console.log(hour)
+
+
+
+//Get current date
+function dateGetter () {
+    currentDay.text(moment().format('[Today is] MMM Do YYYY'))
+}
+
+dateGetter()
+
+//if statement on selecting current hour block
+
+
